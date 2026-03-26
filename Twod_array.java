d95e88edd629229f8e45a1f52f512e41449279b4
S kiarn <soudrikiran@gmail.com>
2026-03-26 22:37:29 +0530

    Add files via upload

---
+++ Twod_array.java
@@ -0,0 +1,13 @@
+public class Twod_array { 
+    public static void main(String[] args) {
+        int[][] matrix1={ {1,2,3}, {4,5,6}, {7,8,9} };
+        for(int i=0;i < matrix1.length;i++){
+            for(int j=0;j<matrix1[0].length;j++){
+                System.out.print(matrix1[i][j] +" ");
+
+            }
+            System.out.println();
+        }
+    
+    }
+}
